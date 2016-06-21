ca5852d0e65bfc877e62f6ff6bb4bf8de8739c64
CodeDotJS <rushforlinux@gmail.com>
2016-06-22 05:13:56 +0530

    clean

---
+++ index.js
@@ -0,0 +1,130 @@
+'use strict';
+
+var got = require('got');
+var cheerio = require('cheerio');
+var Promise = require('pinkie-promise');
+
+function gitPinned(username) {
+	if (typeof username !== 'string') {
+		return Promise.reject(new Error('username required'));
+	}
+	var url = 'https://github.com/' + username;
+	return got(url).then(function (res) {
+		var $ = cheerio.load(res.body);
+		return {
+			repo1: $('.repo-and-owner').eq(0).text().trim() || null,
+			repo2: $('.repo-and-owner').eq(1).text().trim() || null,
+			repo3: $('.repo-and-owner').eq(2).text().trim() || null,
+			repo4: $('.repo-and-owner').eq(3).text().trim() || null,
+			repo5: $('.repo-and-owner').eq(4).text().trim() || null
+		};
+	}).catch(function (err) {
+		if (err.statusCode === 404) {
+			err.message = 'user doesn\'t exists';
+		}
+		throw err;
+	});
+}
+
+function gitYear(username) {
+	if (typeof username !== 'string') {
+		return Promise.reject(new Error('username required'));
+	}
+	var url = 'https://github.com/' + username;
+	return got(url).then(function (res) {
+		var $ = cheerio.load(res.body);
+		return $('h3').eq(2).text().trim() || null;
+	}).catch(function (err) {
+		if (err.statusCode === 404) {
+			err.message = 'user doesn\'t exists';
+		}
+		throw err;
+	});
+}
+
+function gitDay(username) {
+	if (typeof username !== 'string') {
+		return Promise.reject(new Error('username required'));
+	}
+	var url = 'https://github.com/' + username + '?tab=overview&period=daily';
+	return got(url).then(function (res) {
+		var $ = cheerio.load(res.body);
+		return {
+			commits: $('h3').eq(3).text().trim() || null,
+			pulls: $('h3').eq(4).text().trim() || null,
+			issues: $('h3').eq(5).text().trim() || null
+		};
+	}).catch(function (err) {
+		if (err.statusCode === 404) {
+			err.message = 'user doesn\'t exists';
+		}
+		throw err;
+	});
+}
+
+function gitDays(username) {
+	if (typeof username !== 'string') {
+		return Promise.reject(new Error('username required'));
+	}
+	var url = 'https://github.com/' + username + '?tab=overview&period=halfweekly';
+	return got(url).then(function (res) {
+		var $ = cheerio.load(res.body);
+		return {
+			commits: $('h3').eq(3).text().trim() || null,
+			pulls: $('h3').eq(4).text().trim() || null,
+			issues: $('h3').eq(5).text().trim() || null
+		};
+	}).catch(function (err) {
+		if (err.statusCode === 404) {
+			err.message = 'user doesn\'t exists';
+		}
+		throw err;
+	});
+}
+
+function gitWeek(username) {
+	if (typeof username !== 'string') {
+		return Promise.reject(new Error('username required'));
+	}
+	var url = 'https://github.com/' + username + '?tab=overview&period=weekly';
+	return got(url).then(function (res) {
+		var $ = cheerio.load(res.body);
+		return {
+			commits: $('h3').eq(3).text().trim() || null,
+			pulls: $('h3').eq(4).text().trim() || null,
+			issues: $('h3').eq(5).text().trim() || null
+		};
+	}).catch(function (err) {
+		if (err.statusCode === 404) {
+			err.message = 'user doesn\'t exists';
+		}
+		throw err;
+	});
+}
+
+function gitMonth(username) {
+	if (typeof username !== 'string') {
+		return Promise.reject(new Error('username required'));
+	}
+	var url = 'https://github.com/' + username + '?tab=overview&period=monthly';
+	return got(url).then(function (res) {
+		var $ = cheerio.load(res.body);
+		return {
+			commits: $('h3').eq(3).text().trim() || null,
+			pulls: $('h3').eq(4).text().trim() || null,
+			issues: $('h3').eq(5).text().trim() || null
+		};
+	}).catch(function (err) {
+		if (err.statusCode === 404) {
+			err.message = 'user doesn\'t exists';
+		}
+		throw err;
+	});
+}
+
+exports.pin = gitPinned;
+exports.year = gitYear;
+exports.day = gitDay;
+exports.days = gitDays;
+exports.week = gitWeek;
+exports.month = gitMonth;
---
+++ test.js
@@ -0,0 +1,35 @@
+import test from 'ava';
+import fn from './';
+
+test(async t => {
+	const gitDay = await fn.day('codedotjs');
+	const gitDays = await fn.days('codedotjs');
+	const gitWeek = await fn.week('codedotjs');
+	const gitMonth = await fn.month('codedotjs');
+	const gitYear = await fn.year('codedotjs');
+	const gitPin = await fn.pin('codedotjs');
+
+	t.is(gitDay.commits, '9 commits');
+	t.is(gitDay.pulls, null);
+	t.is(gitDay.issues, null);
+
+	t.is(gitDays.commits, '38 commits');
+	t.is(gitDays.pulls, null);
+	t.is(gitDays.issues, null);
+
+	t.is(gitWeek.commits, '62 commits');
+	t.is(gitWeek.pulls, '3 Pull Requests');
+	t.is(gitWeek.issues, '1 Issue reported');
+
+	t.is(gitMonth.commits, '211 commits');
+	t.is(gitMonth.pulls, '12 Pull Requests');
+	t.is(gitMonth.issues, '6 Issues reported');
+
+	t.is(gitYear, '1,793 contributions in the last year');
+
+	t.is(gitPin.repo1, 'instavim');
+	t.is(gitPin.repo2, 'facebook-mass-comment');
+	t.is(gitPin.repo3, 'feross/nagger');
+	t.is(gitPin.repo4, 'cauealves/pagespeed-score-cli');
+	t.is(gitPin.repo5, 'sindresorhus/fast-cli');
+});
